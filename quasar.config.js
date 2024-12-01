c32b44741a7ba8e3e82ab8826c6b088cb0e595e7
Razan Muhammad <razanmfs507@gmail.com>
2024-12-01 23:25:25 +0700

    change port localhost

--- quasar.config.js
+++ quasar.config.js
@@ -10,7 +10,7 @@
 
 const { configure } = require("quasar/wrappers");
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function (ctx) {
   return {
     eslint: {
       // fix: true,
@@ -81,6 +81,7 @@ module.exports = configure(function (/* ctx */) {
     devServer: {
       // https: true
       host: "localhost",
+      port: ctx.mode.spa ? 8000 : ctx.mode.pwa ? 9000 : 9090,
       open: true, // opens browser window automatically
     },
 
