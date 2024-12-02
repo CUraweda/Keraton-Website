7bacc8e7c9f4d30c0d6e9e5033a9b5ad29598321
Nur Cahyanto <nur.razdan@gmail.com>
2024-12-02 14:25:09 +0700

    update

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import bg from "../assets/images/bg1.png";
+import ReactPlayer from "react-player";
 
 const Homepage = () => {
   return (
@@ -26,18 +27,28 @@ const Homepage = () => {
           </div>
         </div>
         {/* //tentang section */}
-        <div
-          className="w-full"
-          style={{
-            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${bg})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-            <div>
-                <p className="text-yellow-400 text-xl">Tentang</p>
-            </div>
+        <div className="w-full py-5 flex flex-col sm:flex-row justify-center items-center px-5 gap-2">
+          <div className="w-full sm:w-1/2 flex flex-col justify-center items-center gap-3 text-center">
+            <p className="text-yellow-400 text-xl">Tentang</p>
+            <p className="text-5xl">Keraton Kesepuhan Cirebon</p>
+            <p className="text-sm">
+              Pada awai pembangunannya. Keraton Kasepuhan dibangun oleh Pangeran
+              Emas Zainul Arifin dengan maksud untuk memperluas bangunan
+              pesanggerahan Keraton Pangkuwati
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 flex flex-col justify-center items-center gap-3 text-center">
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=XdiSL6fzW-Q"
+              controls
+              width="100%"
+              
+            />
+          </div>
         </div>
+
+        {/* section video youtube */}
+        <div className="w-full p-5 "></div>
       </div>
     </>
   );
